07cefdd9c63ccbe8abe84e560efd91301d20c03f
chokoladis <igor2011__igor@mail.ru>
2025-07-28 23:57:22 +0400

    add listener keyboard, custom error, warnings, buety return rows, correct write date to db

--- go.mod
+++ go.mod
@@ -3,7 +3,10 @@ module github.com/chokoladis/GO_projects
 go 1.24.5
 
 require (
+	atomicgo.dev/keyboard v0.2.9 // indirect
 	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/containerd/console v1.0.3 // indirect
 	github.com/go-sql-driver/mysql v1.9.3 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
+	golang.org/x/sys v0.0.0-20220319134239-a9b59b0215f8 // indirect
 )
--- todolist.go
+++ todolist.go
@@ -5,13 +5,19 @@ import (
 	"fmt"
 	"log"
 	"os"
-    "time"
+	"time"
 
+	"atomicgo.dev/keyboard"
+	"atomicgo.dev/keyboard/keys"
 	"github.com/go-sql-driver/mysql"
-    "github.com/joho/godotenv"
+	"github.com/joho/godotenv"
 )
 
-const TABLE_NAME string = "tasks"
+const TABLE_NAME = "tasks"
+
+const COLOR_DEFAULT = "\033[0m"
+const COLOR_RED = "\033[31m"
+const COLOR_YELLOW = "\033[33m"
 
 var db *sql.DB = nil
 
@@ -36,20 +42,48 @@ func main(){
 
     fmt.Scan(&actionId)
 
+    //todo
+    keyboard.Listen(func(key keys.Key) (stop bool, err error) {
+        
+        if key.Code == keys.CtrlC {
+            return true, nil
+        } else {
+            actionId = int(key.Code)
+        }
+
+        return false, nil
+    })
+
+    if (actionId == 0){
+        fmt.Println("You sure to exit? (+,-,y,n)")
+        var accept string
+        fmt.Scan(&accept)
+        if (accept == "+" || accept == "y") {
+            return
+        }
+    }
+
     for (actionId != 0){
         actionId = makeAction(actionId)
     }    
 }
 
+
 func makeAction(actionId int) int {
-    
-    colorYellow := "\033[33m"
 
     switch actionId {
         case 1:
             tasks := getTasks()
             if (len(tasks) < 1){
-                fmt.Println(string(colorYellow), "--- tasks not found")
+                fmt.Println(string(COLOR_YELLOW), "[warning]! Tasks not found")
+                fmt.Printf(string(COLOR_DEFAULT))
+            } else {
+                
+                fmt.Println("ID", "NAME", "COMPLITE", "DATE_START", "DATE_END")
+
+                for _,task := range tasks {
+                    fmt.Println(task.ID, task.NAME, task.COMPLITE, task.DATE_START, task.DATE_END)
+                }
             }
         case 2:
             add()
@@ -80,10 +114,10 @@ func getDB() *sql.DB {
     cfg.Addr = os.Getenv("DB_HOST")+":"+os.Getenv("DB_PORT")
     cfg.DBName = os.Getenv("DB_DATABASE")
 
-    db, err := sql.Open("mysql", cfg.FormatDSN())
+    db, err := sql.Open("mysql", cfg.FormatDSN()+"?parseTime=true")
     if err != nil {
+        fmt.Println(string(COLOR_RED), "[error]! connect to database")
         log.Fatal(err)
-        fmt.Println("Обнаружена ошибка подключения к базе")
     }
     return db
 }
@@ -91,32 +125,37 @@ func getDB() *sql.DB {
 func getTasks() []Task {
     var tasks []Task
     rows, err := db.Query("SELECT * FROM "+TABLE_NAME)
+    defer rows.Close()
+
     if (err != nil) {
-        fmt.Println("custom err - ", err)
+        fmt.Println(string(COLOR_RED), "[error] get list tasks - ", err)
     } else {
         for rows.Next() {
             var task Task
-            if err := rows.Scan(&task.ID, &task.NAME, &task.COMPLITE,
-                &task.DATE_START, &task.DATE_END, &task.DATE_INSERT); err != nil {
-                break
+            err := rows.Scan(&task.ID, &task.NAME, &task.COMPLITE,
+                &task.DATE_START, &task.DATE_END, &task.DATE_INSERT);
+
+            if err != nil {
+                fmt.Println(string(COLOR_RED), "[error] reading task - ",err)
+                continue
             }
             tasks = append(tasks, task)
         }
     }
 
-    defer rows.Close()
+    
     return tasks
 }
 
 func add(){
     
-    var complite string 
+    var complite, dateStart, dateEnd string 
     var taskObj Task
 
-    fmt.Println("enter name taks")
+    fmt.Println("Enter name task")
     fmt.Scan(&taskObj.NAME)
 
-    fmt.Println("enter complited task or not (+,-,y,n)")
+    fmt.Println("Enter complited task or not (+,-,y,n)")
     fmt.Scan(&complite)
 
     if (complite == "+" || complite == "y"){
@@ -125,16 +164,35 @@ func add(){
         taskObj.COMPLITE = false
     }
 
-    taskObj.DATE_START = time.Now()
-    taskObj.DATE_END = time.Now()
-    fmt.Println("enter date start *format(2000-01-01)")
-    fmt.Scan(&taskObj.DATE_START)
+    fmt.Println("Enter date start *format(01.01.2000)")
+    fmt.Scan(&dateStart)
 
-    result, error := db.Exec("INSERT INTO "+TABLE_NAME+" (NAME, COMPLITE, DATE_START, DATE_END) VALUES(?, ?, ?, ?)", 
+    fmt.Println("Enter date end")
+    fmt.Scan(&dateEnd)
+
+    layout := "02.01.2006"
+
+	parsedDateStart, err := time.Parse(layout, dateStart)
+	if err != nil {
+		fmt.Println(string(COLOR_RED), "[error] parsing:", err)
+        fmt.Println(string(COLOR_DEFAULT))
+		return
+	}
+
+    parsedDateEnd, err := time.Parse(layout, dateEnd)
+	if err != nil {
+		fmt.Println(string(COLOR_RED),"[error] parsing:", err)
+        fmt.Println(string(COLOR_DEFAULT))
+		return
+	}
+
+	taskObj.DATE_START = parsedDateStart
+    taskObj.DATE_END = parsedDateEnd
+
+    _, error := db.Exec("INSERT INTO "+TABLE_NAME+" (NAME, COMPLITE, DATE_START, DATE_END) VALUES(?, ?, ?, ?)", 
         taskObj.NAME, taskObj.COMPLITE, taskObj.DATE_START, taskObj.DATE_END)
     if (error != nil) {
         log.Fatal(error)
         return
     }
-    fmt.Printf("result: %v\n", result)
 }
